69675b607ab6d55900538e055f41402dadda04f7
BingNa Yang <bingnayang@gmail.com>
2020-06-30 13:53:52 -0400

    add methods to query BooksTitle and All Book Info

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,10 @@
 package com.company;
 
 import com.company.model.Author;
+import com.company.model.Book;
 import com.company.model.Datasource;
+
+import java.sql.*;
 import java.util.List;
 
 public class Main {
@@ -13,15 +16,43 @@ public class Main {
             return;
         }
 
-        List<Author> authors = datasource.queryAuthors();
+        // Get Authors Name
+        System.out.println("===List Authors in DataBase===");
+        List<Author> authors = datasource.queryAuthors(Datasource.ORDER_BY_NONE);
         if(authors == null){
             System.out.println("No Author in database");
             return;
         }
         for(Author author : authors){
-            System.out.println("ID: "+author.getAuthor_ID()+", Author Name: "+author.getAuthor_name());
+            System.out.println("Author Name: "+author.getAuthor_name());
         }
 
+        // Get Book Title
+        System.out.println("===List Book Title in DataBase===");
+        List<Book> books = datasource.queryBooksTitle(Datasource.ORDER_BY_NONE);
+        if(books == null){
+            System.out.println("No book in database");
+            return;
+        }
+        for(Book book: books){
+            System.out.println("Book Title: "+book.getBook_name());
+        }
+
+        // Get Book Title
+        System.out.println("===List Book Info in DataBase===");
+        List<String> bookInfo = datasource.queryAllBooksInfo(Datasource.ORDER_BY_NONE);
+        if(books == null){
+            System.out.println("No book in database");
+            return;
+        }
+        for(String book: bookInfo){
+            System.out.println(book);
+        }
+
+
+
         datasource.close();
+
+
     } // end: main
 }
--- src/com/company/model/Datasource.java
+++ src/com/company/model/Datasource.java
@@ -1,5 +1,6 @@
 package com.company.model;
 
+import javax.xml.transform.stream.StreamResult;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +37,11 @@ public class Datasource {
     public static final int INDEX_PUBLISHER_ID = 1;
     public static final int INDEX_PUBLISHER_NAME = 2;
 
+    public static final int ORDER_BY_NONE = 1;
+    public static final int ORDER_BY_ASC = 2;
+    public static final int ORDER_BY_DESC = 3;
+
+
     private Connection connection;
     public boolean open(){
         try{
@@ -56,15 +62,27 @@ public class Datasource {
         }
     }
 
-    public List<Author> queryAuthors(){
-        // Use try-with-resource will automacally close both statement and resultset
+    public List<Author> queryAuthors(int sortOrder){
+        StringBuilder stringBuilder = new StringBuilder("SELECT * FROM ");
+        stringBuilder.append(TABLE_AUTHORS);
+        if(sortOrder != ORDER_BY_NONE){
+            stringBuilder.append(" ORDER BY ");
+            stringBuilder.append(COLUMN_AUTHOR_NAME);
+            stringBuilder.append(" COLLATE NOCASE ");
+            if(sortOrder == ORDER_BY_DESC){
+                stringBuilder.append("DESC");
+            }else {
+                stringBuilder.append("ASC");
+            }
+        }
+
+        // Use try-with-resource will automatic close both Statement and ResultSet
         try(Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM "+TABLE_AUTHORS)){
+            ResultSet resultSet = statement.executeQuery(stringBuilder.toString())){
 
             List<Author> authors = new ArrayList<>();
             while(resultSet.next()){
                 Author author = new Author();
-                author.setAuthor_ID(resultSet.getInt(INDEX_AUTHOR_ID));
                 author.setAuthor_name(resultSet.getString(INDEX_AUTHOR_NAME));
                 authors.add(author);
             }
@@ -74,5 +92,83 @@ public class Datasource {
             return null;
         }
     } // end: queryAuthors
-    
+
+    public List<Book> queryBooksTitle(int sortOrder){
+        StringBuilder stringBuilder = new StringBuilder("SELECT * FROM ");
+        stringBuilder.append(TABLE_BOOKS);
+        if(sortOrder != ORDER_BY_NONE){
+            stringBuilder.append(" ORDER BY ");
+            stringBuilder.append(COLUMN_AUTHOR_NAME);
+            stringBuilder.append(" COLLATE NOCASE ");
+            if(sortOrder == ORDER_BY_DESC){
+                stringBuilder.append("DESC");
+            }else {
+                stringBuilder.append("ASC");
+            }
+        }
+
+        // Use try-with-resource will automatic close both Statement and ResultSet
+        try(Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(stringBuilder.toString())){
+
+            List<Book> books = new ArrayList<>();
+            while(resultSet.next()){
+                Book book = new Book();
+                book.setBook_name(resultSet.getString(INDEX_AUTHOR_NAME));
+                books.add(book);
+            }
+            return books;
+        }catch (SQLException e){
+            System.out.println("Query failed: "+e.getMessage());
+            return null;
+        }
+    }// end: queryBooks
+
+    public List<String> queryAllBooksInfo(int sortOrder){
+//        SELECT book_name, author_name,book_ISBN_13,book_edition,book_language,publisher_name,book_publication_date
+//        FROM books
+//        INNER JOIN authors
+//        ON books.book_author_ID = authors.author_ID
+//        INNER JOIN publishers
+//        ON books.book_publisher_ID = publishers.publisher_ID
+
+        StringBuilder stringBuilder = new StringBuilder("SELECT "+
+                COLUMN_BOOK_NAME+","+
+                COLUMN_AUTHOR_NAME+","+
+                COLUMN_BOOK_ISBN_13+","+
+                COLUMN_BOOK_EDITION+","+
+                COLUMN_BOOK_LANGUAGE+","+
+                COLUMN_PUBLISHER_NAME+","+
+                COLUMN_BOOK_PUBLICATION_DATE);
+        stringBuilder.append(" FROM "+TABLE_BOOKS);
+        stringBuilder.append(" INNER JOIN "+TABLE_AUTHORS);
+        stringBuilder.append(" ON "+TABLE_BOOKS+"."+COLUMN_BOOK_AUTHOR_ID +"="+TABLE_AUTHORS+"."+COLUMN_AUTHOR_ID);
+        stringBuilder.append(" INNER JOIN "+TABLE_PUBLISHER);
+        stringBuilder.append(" ON "+TABLE_BOOKS+"."+COLUMN_BOOK_PUBLISHER_ID +"="+TABLE_PUBLISHER+"."+COLUMN_PUBLISHER_ID);
+
+        System.out.println("SQL statement: "+stringBuilder.toString()+"\n");
+
+        // Use try-with-resource will automatic close both Statement and ResultSet
+        try(Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(stringBuilder.toString())){
+
+            List<String> books = new ArrayList<>();
+            while(resultSet.next()){
+                books.add("Book Name: "+resultSet.getString(1)+
+                        "\nAuthor: "+resultSet.getString(2)+
+                        "\nISBN-13: "+resultSet.getString(3)+
+                        "\nEdition: "+resultSet.getInt(4)+
+                        "\nLanguage: "+resultSet.getString(5)+
+                        "\nPublisher: "+resultSet.getString(6)+
+                        "\nPublication Date: "+resultSet.getString(7)+"\n");
+
+            }
+            return books;
+        }catch (SQLException e){
+            System.out.println("Query failed: "+e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+
+    }// end: queryBookInfo
 }
