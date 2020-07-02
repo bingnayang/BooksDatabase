58e963b9ae4d0fc54dd0cbc3c9d33688941d05c3
BingNa Yang <bingnayang@gmail.com>
2020-07-02 18:47:15 -0400

    fix menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import com.company.model.Author;
 import com.company.model.Book;
 import com.company.model.Datasource;
+
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,90 +11,102 @@ public class Main {
 
     public static void main(String[] args) {
         Datasource datasource = new Datasource();
-
-        if(!datasource.open()){
+        Scanner scanner = new Scanner(System.in);
+        if (!datasource.open()) {
             System.out.println("Cannot open datasource");
             return;
         }
-
-        startMenu();
-        Scanner scanner = new Scanner(System.in);
-        int option = scanner.nextInt();
-        switch (option){
-            case 1:
-                displayAllAuthorsName(datasource);
-                break;
-            case 2:
-                displayAllBookTitle(datasource);
-                break;
-            case 3:
-                displayAllBookInfo(datasource);
-                break;
-            case 4:
-                searchBooksByAuthor(datasource);
-                break;
-            default:
-                System.out.println("Not an option");
-                break;
-        }
-
+        int option;
+        do {
+            startMenu();
+            option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    displayAllAuthorsName(datasource);
+                    break;
+                case 2:
+                    displayAllBookTitle(datasource);
+                    break;
+                case 3:
+                    displayAllBookInfo(datasource);
+                    break;
+                case 4:
+                    searchBooksByAuthor(datasource);
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("Not an option");
+                    break;
+            }
+        } while (option != 5);
         datasource.close();
     } // end: main
-    public static void startMenu(){
+
+    public static void startMenu() {
         System.out.println("========Book Database========");
         System.out.println("1) List All Authors in Database");
         System.out.println("2) List All Books Title in DataBase");
         System.out.println("3) List All Books Info in DataBase");
         System.out.println("4) List Books Title By Author");
+        System.out.println("5) Exit");
         System.out.print("Enter Your Option: ");
         System.out.println();
 
     }
-    public static void displayAllAuthorsName(Datasource datasource){
+
+    public static void displayAllAuthorsName(Datasource datasource) {
         // Get Authors Name
         System.out.println("===List Authors in DataBase===");
         List<Author> authors = datasource.queryAuthors(Datasource.ORDER_BY_NONE);
-        if(authors == null){
+        if (authors == null) {
             System.out.println("No Author in database");
             return;
         }
-        for(Author author : authors){
-            System.out.println("Author Name: "+author.getAuthor_name());
+        for (Author author : authors) {
+            System.out.println("Author Name: " + author.getAuthor_name());
         }
     }
-    public static void displayAllBookTitle(Datasource datasource){
+
+    public static void displayAllBookTitle(Datasource datasource) {
         // Get Book Title
         System.out.println("===List Books Title in DataBase===");
         List<Book> books = datasource.queryBooksTitle(Datasource.ORDER_BY_NONE);
-        if(books == null){
+        if (books == null) {
             System.out.println("No book in database");
             return;
         }
-        for(Book book: books){
-            System.out.println("Book Title: "+book.getBook_name());
+        for (Book book : books) {
+            System.out.println("Book Title: " + book.getBook_name());
         }
     }
+
     public static void displayAllBookInfo(Datasource datasource) {
         // Get All Book Info
         System.out.println("===List All Books Info in DataBase===");
         List<String> bookInfo = datasource.queryAllBooksInfo(Datasource.ORDER_BY_NONE);
-        if(bookInfo == null){
+        if (bookInfo == null) {
             System.out.println("No book in database");
             return;
         }
-        for(String book: bookInfo){
+        for (String book : bookInfo) {
             System.out.println(book);
         }
     }
+
     public static void searchBooksByAuthor(Datasource datasource) {
         // Get Books Title by Author
+        Scanner scanner = new Scanner(System.in);
         System.out.println("===List Books Title By Author===");
-        List<String> booksByAuthor = datasource.queryBooksTitleByAuthorName("Ernest Hemingway");
-        if(booksByAuthor == null){
+        System.out.println("Enter Author Name: ");
+        String authorName = scanner.nextLine();
+
+        List<String> booksByAuthor = datasource.queryBooksTitleByAuthorName(authorName);
+        if (booksByAuthor == null) {
             System.out.println("No such data");
             return;
         }
-        for(String bookByAuthor: booksByAuthor){
+        for (String bookByAuthor : booksByAuthor) {
             System.out.println(bookByAuthor);
         }
     }
